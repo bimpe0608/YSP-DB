ae4a669ce17fff1f8d65f065d0a9fc74f137e057
bimpe <adeyinminuadebimpe@gmail.com>
2023-04-17 14:16:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const yspStaff = [
+    {name:"Bimpe", height:"5'11", weight:"58kg", gender:"Female", role:"Frontend/social media", officeRating:"10", salaryExpectation:"100,000"},
+    {name:"Tobe", height:"5'8", weight:"75kg", gender:"Male", role:"Frontend dev", officeRating:"2", salaryExpectation:"30,000"},
+    {name:"Ore", height:"5'2", weight:"40kg", gender:"Female", role:"Front desk", officeRating:"7", salaryExpectation:"50,000"},
+    {name:"David", height:"5'11", weight:"70kg", gender:"Male", role:"Media", officeRating:"7", salaryExpectation:"70,000"},
+    {name:"Mojeed", height:"5'8", weight:"60kg", gender:"Male", role:"Math tutor/Head of operations", officeRating:"8.5", salaryExpectation:"100,000"},
+    {name:"Deola", height:"5'7", weight:"70kg", gender:"Female", role:"UI/UX", officeRating:"9", salaryExpectation:"150,000"},
+    {name:"Oluchi", height:"5'11", weight:"78kg", gender:"Female", role:"Head of finance", officeRating:"9", salaryExpectation:"150,000"},
+    {name:"Owujib", height:"5'11", weight:"80kg", gender:"Male", role:"Fullstack dev", officeRating:"10", salaryExpectation:"200,000"},
+    {name:"Layi", height:"5'11", weight:"65kg", gender:"Male", role:"Brand designer", officeRating:"7.5", salaryExpectation:"100,000"},
+    {name:"Deba", height:"6'0", weight:"90kg", gender:"Male", role:"Senior dev", officeRating:"10", salaryExpectation:"300,000"},
+    {name:"Austin", height:"5'6", weight:"60kg", gender:"Male", role:"Media/Editing", officeRating:"7.5", salaryExpectation:"70,000"},
+    {name:"Bolaji", height:"5'11", weight:"70kg", gender:"Male", role:"Media/Editing", officeRating:"7.5", salaryExpectation:"70,000"},
+    {name:"Innocent", height:"6'0", weight:"85kg", gender:"Male", role:"Co-founder/Creative director", officeRating:"10", salaryExpectation:"500,000"},
+    {name:"Daniel", height:"5'6", weight:"70kg", gender:"Male", role:"CEO", officeRating:"10", salaryExpectation:"500,000"}, 
+    {name:"Hamzat", height:"5'6.5", weight:"65kg", gender:"Male", role:"Backend", officeRating:"5", salaryExpectation:"100,000"},
+];
+
+console.log(yspStaff);
+
+
